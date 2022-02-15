5826928ae0ad6516447149983d68aa1f1946ac76
Meerwaldten <mikkell_m_j@hotmail.com>
2022-02-15 16:23:42 +0100

    Hej

---
+++ src/test/java/com/example/springconverter/SpringConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
